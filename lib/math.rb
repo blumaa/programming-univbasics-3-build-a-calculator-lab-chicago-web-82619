cc8ac36355a1f80bcac29876070c02b14c8d303e
Aaron Blum <blumaa@gmail.com>
2019-07-25 16:45:28 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,25 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  return sum
 end
 
 def subtraction(num1, num2)
-
+  sub = num1 - num2
+  return sub
 end
 
 def division(num1, num2)
-
+  div = num1 / num2
+  return div
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  return product
 end
 
 def modulo(num1, num2)
-
+  
 end
 
 def square_root(num)
